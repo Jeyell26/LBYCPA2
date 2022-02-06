ad35a3ade881c44091e15d67af8b03e8c9b95f17
kyoseia <87558896+kyoseia@users.noreply.github.com>
2022-02-06 23:38:10 +0800

    Implemented most of the BST, subject to change depending on how we use the BST for the database and program.

--- src/BST/BSTree.java
+++ src/BST/BSTree.java
@@ -1,4 +1,136 @@
 package BST;
 
+import java.util.Objects;
+
 public class BSTree {
+    public Node root;
+    public boolean flag;
+    public int height;
+
+    public BSTree(){
+        root = null;
+    }
+
+    public static class Node {
+        public String title;
+        public String definition;
+        public Node parent;
+        public Node left;
+        public Node right;
+
+        Node(String item, String def){
+            title = item;
+            definition = def;
+            parent = null;
+            left = null;
+            right = null;
+        }
+
+        public Node getSibling(){
+            Node temp = this.parent;
+            if (this == temp.right){
+                return temp.left;
+            } else return temp.right;
+        }
+    }
+
+    public Boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(String item, String def){
+        Node temp;
+        temp = new Node(item, def);
+        if (root == null) {
+            height = 1;
+            root = temp;
+        }
+        else{
+            Node curr = root;
+            while (curr != null) {
+                temp.parent = curr;
+                if (temp.title.compareToIgnoreCase(curr.title) > 0) curr = curr.right;
+                else curr = curr.left;
+                }
+            if (temp.title.compareToIgnoreCase(temp.parent.title) > 0) temp.parent.right = temp;
+            else temp.parent.left = temp;
+        }
+    }
+
+    public void delete(String item){
+        this.root = deletion(root,item);
+    }
+
+    Node deletion(Node root, String item){
+        if (root == null){
+            return null;
+        }
+        else if(item.compareToIgnoreCase(root.title) < 0)
+            root.left = deletion(root.left,item);
+        else if(item.compareToIgnoreCase(root.title) > 0)
+            root.right = deletion(root.right,item);
+        else{
+            if (root.left == null && root.right == null){
+                return null;
+            }
+            else if(root.left == null){
+                Node parent = root.parent;
+                root = root.right;
+                root.parent = parent;
+            }
+            else if(root.right == null){
+                Node parent = root.parent;
+                root = root.left;
+                root.parent = parent;
+            }
+            else {
+                Node temp = minimum(root.right);
+                root.title = temp.title;
+                root.right = deletion(root.right,temp.title);
+            }
+        }
+        return root;
+    }
+
+    Node minimum(Node root)
+    {
+        while (root.left != null)
+        {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public Node search(String item){
+        if (root == null){
+            System.out.println("Tree is empty");
+            return root;
+        }
+        Node curr = root;
+        while (curr != null)
+        {
+            if (Objects.equals(curr.title, item)) {
+                break;
+            } else {
+                if (item.compareToIgnoreCase(curr.title) > 0)
+                    curr = curr.right;
+                else curr = curr.left;
+            }
+        }
+        return curr;
+    }
+
+    String inorder(Node n){
+        String str = "";
+        if (n!=null){
+            str += inorder(n.left);
+            str += n.title + " ";
+            str += inorder(n.right);
+        }
+        return str;
+    }
+
+
+
+
 }
